fa1c9e80457b0424fa0348626983238366f185b1
olivier-twist <32522183+olivier-twist@users.noreply.github.com>
2025-09-18 05:03:31 +0000

    Final commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
 	mux := http.NewServeMux()
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", Home)
 	mux.HandleFunc("/snippetbox/create", CreateSnippet)
 	mux.HandleFunc("/snippetbox/view", ViewSnippet)
